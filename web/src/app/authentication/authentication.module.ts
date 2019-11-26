21afd072a3162bc96bd00776b66da955799d53ca
Dan Hutchinson <danielhutchinson5@gmail.com>
2019-11-26 20:45:25 +0000

    Add Nest server + docker setup
    Quick test to make the web call the server

--- web/src/app/authentication/authentication.module.ts
+++ web/src/app/authentication/authentication.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { AuthenticationContainerComponent } from "./containers/authentication-container/authentication-container.component";
 import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [AuthenticationContainerComponent],
-  imports: [ReactiveFormsModule]
+  imports: [ReactiveFormsModule, HttpClientModule, BrowserModule]
 })
 export class AuthenticationModule {}
--- web/src/app/authentication/containers/authentication-container/authentication-container.component.ts
+++ web/src/app/authentication/containers/authentication-container/authentication-container.component.ts
@@ -1,7 +1,17 @@
 import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Component({
   templateUrl: "./authentication-container.component.html",
   styleUrls: ["./authentication-container.component.scss"]
 })
-export class AuthenticationContainerComponent {}
+export class AuthenticationContainerComponent implements OnInit {
+  message: Observable<any>;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.message = this.httpClient.get("http://localhost:3000");
+  }
+}
