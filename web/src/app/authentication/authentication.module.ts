04adfa77f6dab8950bccf57e021801e046fcf045
Dan Hutchinson <danielhutchinson5@gmail.com>
2019-12-01 14:55:27 +0000

    web: refactor login form into its own component

--- web/src/app/authentication/authentication.module.ts
+++ web/src/app/authentication/authentication.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from '../shared/shared.module';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
-  declarations: [AuthenticationContainerComponent],
+  declarations: [AuthenticationContainerComponent, LoginFormComponent],
   imports: [ReactiveFormsModule, HttpClientModule, BrowserModule, SharedModule],
 })
 export class AuthenticationModule {}
